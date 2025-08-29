cc5885db3fc0e8e5e8bb815bbf7b44fbf2f7071b
chitrung252 <trung.ktpm@gmail.com>
2025-08-29 15:04:55 +0700

    refactor: code

--- src/common/components/sections/FrameSection.tsx
+++ src/common/components/sections/FrameSection.tsx
@@ -1,24 +1,11 @@
-import { useEffect, useRef, useState } from 'react';
 import metadata from '../../../data/metadata.json';
 import { ImageCommon } from '../common';
-import { useCheckMobileScreen } from '../../hooks';
 
 const className = 'st-frame-section';
 
 const FrameSection: React.FC = () => {
-  const isMobile = useCheckMobileScreen();
-
-  // Entrance animation when section comes into view
-
   return (
     <div className={`${className}__container`}>
-      {/* <div className={`${className}__container__bg-image`}>
-        <ImageCommon
-          src={metadata.sectionFrame.images[0].image}
-          alt={metadata.sectionFrame.images[0].alt}
-        />
-      </div> */}
-
       <div className={`${className}__container__wrap`}>
         <div className={`${className}__container__wrap__description`}>
           {metadata.sectionFrame.description}
--- src/common/components/sections/index.tsx
+++ src/common/components/sections/index.tsx
@@ -1,35 +1,19 @@
-import BlogSection from './BlogSection';
 import CarouselSection from './CarouselSection';
-import CleaningProcessSection from './CleaningProcessSection';
-import HomeSection from './HomeSection';
-import InformationBottomSection from './InformationBottomSection';
 import MenuSection from './MenuSection';
-import MobileBlogSection from './MobileBlogSection';
 import MobileCarouselSection from './MobileCarouselSection';
-import MobileCleaningProcessSection from './MobileCleaningProcessSection';
 import MobileHomeSection from './MobileHomeSection';
 import MobileInformationBottomSection from './MobileInformationBottomSection';
 import MobileNavBarSection from './MobileNavBarSection';
 import MobilePolicySection from './MobilePolicySection';
 import MobileServiceSection from './MobileServiceSection';
 import NavBarSection from './NavBarSection';
-import PolicySection from './PolicySection';
-import ServiceSection from './ServiceSection';
 export {
   NavBarSection,
-  HomeSection,
   CarouselSection,
-  ServiceSection,
-  CleaningProcessSection,
-  BlogSection,
-  PolicySection,
-  InformationBottomSection,
   MobileNavBarSection,
   MobileHomeSection,
   MobileCarouselSection,
   MobileServiceSection,
-  MobileCleaningProcessSection,
-  MobileBlogSection,
   MobileInformationBottomSection,
   MobilePolicySection,
   MenuSection,
